18860294742df678a830bde23277a7e2d7391f9c
Your Name <you@example.com>
2025-02-26 21:02:54 +0400

    first example 26.02.25

---
+++ README.md
@@ -0,0 +1,31 @@
+# Nika - Full Stack Developer
+
+Hi, I'm Nika Full Stack Developer. My main priorities are accuracy, understandable code, and quality projects. 🚀
+
+📫 Contact Me:
+- [LinkedIn](https://www.linkedin.com/in/nika-ebralidze-812431329/)
+- [GitHub](https://github.com/NikaEbraLidze)
+- [Upwork](https://www.upwork.com/freelancers/~0101df07747fc6de4f)
+- 📧 Email: nikaebralidze21@gmail.com
+
+🔧 Teck stack:
+- **Frontend:** HTML, CSS, JavaScript
+- **Backend:** Node.js, Express.js
+- **Other:** REST APIs, Git, npm
+
+!!!I always study new technologies, at now I working on Databases (SQL and postagreSQL) and React.js!!!
+
+
+## 📌 My main projects
+### 1. [Math Learning Platform](https://github.com/NikaEbraLidze/Math-Website)  
+   - 🔗 **Demo:** [Live](https://math-website-zt9d.onrender.com/)  
+   !!!I hosted it for free, so there may be a long delay when it is turned on!!!
+
+### 1. [Blog Website](https://github.com/NikaEbraLidze/Blog-Website)  
+   - 🔗 **Demo:** [Live](hhttps://blog-website-b5ob.onrender.com/)  
+   !!!I hosted it for free, so there may be a long delay when it is turned on!!!
+
+### 3. [Rock-Paper-Scissors Game](https://github.com/NikaEbraLidze/Rock-Paper-Scissors)
+   - 🔗 **Demo:** [Live](https://nikaebralidze.github.io/Rock-Paper-Scissors/)  
+
+⭐ If you liked my portfolio, please leave a star!
---
+++ index.js
@@ -0,0 +1,46 @@
+$(function() {
+    $(".skills-filter button").eq(0).on("click", function(){
+        $(".w").toggleClass("filtered-skills");
+        $(".skills-filter button").eq(0).toggleClass("filtered-skills");
+    });//for filter skills.
+
+    $(".skills-filter button").eq(1).on("click", function(){
+        $(".m").toggleClass("filtered-skills");
+        $(".skills-filter button").eq(1).toggleClass("filtered-skills");
+    });//for filter skills.
+
+    $(".loading").mouseenter(function() {
+        $(".showoffon").fadeIn(0);
+    });
+
+    $(".loading").mouseleave(function() {
+        $(".showoffon").fadeOut(100);
+    });
+
+    $(".for-aimation").mouseenter(function() {
+        $(".showoffonforproject").fadeIn(0);
+    });
+
+    $(".for-aimation").mouseleave(function() {
+        $(".showoffonforproject").fadeOut(100);
+    });
+
+    $(".click").click(function() {
+        $(".for-mobile, .nav-bar-img").toggle();
+
+        let img = $(".click");
+
+        if (img.attr("src") === "./public/images/navbarmenu.png") {
+            img.attr("src", "./public/images/close.png");
+        } else {
+            img.attr("src", "./public/images/navbarmenu.png"); 
+        }
+    });//navbar
+
+    $(document).click(function(event) {
+        if (!$(event.target).closest(".click, .for-mobile").length && $(window).width() < 899) {
+            $(".for-mobile, .nav-bar-img").hide();
+            $(".click").attr("src", "./public/images/navbarmenu.png");
+        }
+    });//navbar close
+});
